21af812520a5b026457bd2b9dfd3440226d12778
The Passionate Programmer <wamungai10@gmail.com>
2023-11-08 06:50:40 -0800

    Configure custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dashboard_bg: "#E7ECEF",
+        dashboard_text: "#8492A6",
+        dashboard_primary: "#C0CCDD",
+        dashboard_stats: "#303133",
+        dashboard_gray: "#6B7280",
+        dashboard_offwhite: "#FCFCFC",
+      },
+    },
   },
   plugins: [],
 }
